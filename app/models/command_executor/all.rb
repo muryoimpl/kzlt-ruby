927f1b6625c4c8bec7956b45f46135d75a2dda75
muryoimpl <muryoimpl@gmail.com>
2024-12-28 03:45:58 +0000

    Add CommandExecutor::All

---
+++ app/models/command_executor/all.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class CommandExecutor
+  class All < Executor
+    def execute
+      @entries = channel.entries.eager_load(:user)
+                                .where(status: %i[unordered ordered delimited])
+                                .order(:id)
+
+      return CommandExecutor::Response.new(message:, is_private: true) if @entries.present?
+      CommandExecutor::Response.new(message: no_entry_message, is_private: true)
+    end
+
+    private
+
+    def message
+      @entries.map do |entry|
+        "- #{badge(entry)}#{entry.title} by #{entry.user.name}, entryId: #{entry.id}"
+      end.join("\n").concat("\n")
+    end
+
+    def no_entry_message
+      "エントリーはありません"
+    end
+
+    def badge(entry)
+      %w[delimited ordered].include?(entry.status) ? "[done] " : ""
+    end
+  end
+end
---
+++ spec/models/command_executor/all_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CommandExecutor::All, type: :model do
+  describe '#execute' do
+    subject { described_class.new(command:, argument:, channel:, user:).execute }
+
+    let(:workspace) { create(:workspace) }
+    let(:channel) { create(:channel, workspace:) }
+    let(:user) { create(:user, name: 'KZRB') }
+
+    let!(:entry1) { create(:entry, user:, channel:) }
+    let!(:entry2) { create(:entry, user:, channel:, status: :ordered) }
+    let!(:entry3) { create(:entry, user:, channel:, status: :removed) }
+    let!(:entry4) { create(:entry, user:, channel:, status: :delimited) }
+
+    let!(:other_channel) { create(:channel, workspace:) }
+    let!(:other_entry1) { create(:entry, user:, channel: other_channel) }
+
+    let(:command) { 'all' }
+    let(:argument) { '無視される' }
+
+    it do
+      expect { subject }.not_to change(Entry, :count)
+    end
+
+    it do
+      response = subject
+
+      msg = <<~TXT
+        - #{entry1.title} by #{entry1.user.name}, entryId: #{entry1.id}
+        - [done] #{entry2.title} by #{entry2.user.name}, entryId: #{entry2.id}
+        - [done] #{entry4.title} by #{entry4.user.name}, entryId: #{entry4.id}
+      TXT
+      expect(response.message).to eq msg
+      expect(response.is_private).to be true
+    end
+
+    context 'エントリがない場合' do
+      before do
+        Entry.destroy_all
+      end
+
+      it do
+        response = subject
+
+        expect(response.message).to eq 'エントリーはありません'
+        expect(response.is_private).to be true
+      end
+    end
+  end
+end
