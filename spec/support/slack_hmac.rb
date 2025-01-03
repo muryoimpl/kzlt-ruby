5d91ba6acea29db14c0c9b3f8e96bee9e033a536
muryoimpl <muryoimpl@gmail.com>
2025-01-03 15:56:48 +0900

    Use URI.encode_www_form_component instead of ERB::Util.u
    空白が `+` か `%20` の違いかによる。Slack の返す結果として、今回は
    `+` と判定されるべきのようであるため、URI.encode_www_form_component
    を使用するように変更する

--- spec/support/slack_hmac.rb
+++ spec/support/slack_hmac.rb
@@ -10,8 +10,9 @@ module SlackRequestVerification
                    :enterprise_name, :channel_id, :channel_name,
                    :user_id, :user_name, :command, :text, :response_url,
                    :trigger_id, :api_app_id
-                 ).map { |k, v| v.nil? ? k.to_s : "#{k}=#{ERB::Util.u(v)}" }.join("&")
+                 ).map { |k, v| v.nil? ? k.to_s : "#{k}=#{URI.encode_www_form_component(v)}" }.join("&")
       base_str = [ ::SlackRequestVerification::VERSION_NUMBER, signatured_at, body_str ].join(":")
+
       hexdigest = OpenSSL::HMAC.hexdigest(
         OpenSSL::Digest.new("sha256"),
         signing_signature,
