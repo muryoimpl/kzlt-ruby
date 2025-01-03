b2468dfefed87e2caf75a47ed9a6a281d7231e1d
muryoimpl <muryoimpl@gmail.com>
2025-01-03 12:23:36 +0900

    Remove unnecessary substitution, and add debug printing if DEBUG == 1
    "+" を "%20" に置換する必要がないことがわかったため削除した。
    ssh を使えないため debug に必要な情報を 環境変数: DEBUG に 1 を入れる
    ことでログに出力するようにした

--- app/controllers/concerns/slack_request_verification.rb
+++ app/controllers/concerns/slack_request_verification.rb
@@ -14,13 +14,23 @@ module SlackRequestVerification
   def verify_request
     signing_signature = ENV["APP_SLACK_SIGNING_SECRET"]
 
-    base_str = [ VERSION_NUMBER, request.headers[TIMESTAMP_KEY], request.body.string.gsub("+", "%20") ].join(":")
+    base_str = [ VERSION_NUMBER, request.headers[TIMESTAMP_KEY], request.body.string ].join(":")
+
     hexdigest = OpenSSL::HMAC.hexdigest(
       OpenSSL::Digest.new("sha256"),
       signing_signature,
       base_str
     )
     my_sig = "#{VERSION_NUMBER}=#{hexdigest}"
+
+    if ENV["DEBUG"].to_i == 1
+      Rails.logger.info("request.body.string: #{request.body.string}")
+      Rails.logger.info("raw_post class: #{request.raw_post.class}, value: #{request.raw_post}, method_symbol: #{request.method_symbol}")
+      Rails.logger.info("my_sig: #{my_sig}")
+      Rails.logger.info("header: x-slack-signature:#{request.headers[SIGNATURE_KEY]}, x-slack-request-timestamp: #{request.headers[TIMESTAMP_KEY]}")
+      params.permit!
+      Rails.logger.info("params: #{params.to_h}")
+    end
 
     render plain: "Forbidden", status: :forbidden if my_sig != request.headers[SIGNATURE_KEY]
   end
