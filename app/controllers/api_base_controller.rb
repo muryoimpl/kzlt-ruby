4b83b2878024b46214eb779c16cd433d913635c7
muryoimpl <muryoimpl@gmail.com>
2025-01-03 14:54:36 +0900

    Print error message and backtrace when occurred exception

--- app/controllers/api_base_controller.rb
+++ app/controllers/api_base_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class ApiBaseController < ApplicationController
+  rescue_from StandardError, with: :render_500
+
   include SlackRequestVerification
   include SlackRequestFilter
+
+  private
+
+  def render_500(e)
+    backtrace_str = e.backtrace.join("\n")
+    render plain: "#{e.class}, #{e.message}: #{backtrace_str}"
+  end
 end
