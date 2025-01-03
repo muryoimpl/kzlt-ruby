68baa4f6b97f1e5ac698e24f5363ab78740bae73
muryoimpl <muryoimpl@gmail.com>
2025-01-03 06:21:15 +0000

    Update help text

--- app/models/help.rb
+++ app/models/help.rb
@@ -2,14 +2,15 @@
 
 class Help
   TEXT = <<~HELP
-    /kzlt entry 'LTタイトル' -- 自分のLTを登録する
-    /kzlt my -- 自分のエントリしたLTを自分にだけ表示する
-    /kzlt list  -- エントリー済みのLTをchannelに出力する(順番を決めたものを除く)
-    /kzlt all -- エントリー済みのLTを出力する(順番決めた/決めてない関係なく)
-    /kzlt shuffle -- 順番を決め、channelに出力する (次のshuffleに出てこない)
-    /kzlt reset -- 順番決めたものすべてを順番決めていないことにする
-    /kzlt remove 'エントリ番号' -- エントリ時に返ってきた番号を指定してエントリを削除する
-    /kzlt delimit -- 一旦区切ってすでに順番を決めたエントリをshuffle対象外とする
+    /kzlt entry <title>    -- 自分のLTを登録する。※ スペースありでも '' で囲む必要なし
+    /kzlt my               -- 自分のエントリしたLTを自分にだけ表示する
+    /kzlt list             -- エントリー済みのLTをchannelに出力する(順番を決めたものを除く)
+    /kzlt all              -- エントリー済みのLTを出力する(順番決めた/決めてない関係なく)
+    /kzlt shuffle          -- 順番を決め、channelに出力する (次のshuffleに出てこない)
+    /kzlt shuffle <seed>   -- 指定した seed で順番を決め、channelに出力する (次のshuffleに出てこない)
+    /kzlt reset            -- 順番決めたものすべてを順番決めていないことにする
+    /kzlt remove <entryId> -- エントリ時に返ってきた番号を指定してエントリを削除する
+    /kzlt delimit          -- 一旦区切ってすでに順番を決めたエントリをshuffle対象外とする
     ※ delimit 後に shuffle することで、前回 shuffle 後にエントリしたものだけで shuffle できます
   HELP
 end
